182ad18dfb4b0c7476b5082406cd605e34abd23b
fatb <arno.tgh@gmail.com>
2020-04-08 02:31:50 +0200

    trying API => it works !!

---
+++ pages/areas.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="(area, index) in areas" :key="index">
+      <h2>{{ area.name }}</h2>
+      <p>
+        Villes :
+        <span v-for="(city) in area.cities" :key="city">
+          {{ city.name }}
+        </span>
+      </p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'areas',
+  async fetch () {
+    const request = await this.$axios.get('/api/areas/')
+    this.areas = request.data
+  },
+  data () {
+    return {
+      areas: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
