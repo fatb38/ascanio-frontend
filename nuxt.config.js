623b1abd19a12045fe2fb0660eeebc368d2c4731
fatb <arno.tgh@gmail.com>
2020-04-08 10:53:24 +0200

    changing font family

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap' }
     ]
   },
   /*
@@ -25,6 +26,7 @@ export default {
   ** Global CSS
   */
   css: [
+    '~/assets/variables.scss'
   ],
   /*
   ** Plugins to load before mounting the App
