afe2cb420a4879b4b92fc53e132ed96b01eb407d
fatb <arno.tgh@gmail.com>
2020-04-08 10:13:09 +0200

    ascanio colors and logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,9 +63,9 @@ export default {
       dark: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
+          primary: '#fabb3a',
           accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
+          secondary: '#2ac0cc',
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
