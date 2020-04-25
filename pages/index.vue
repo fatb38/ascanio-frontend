9f66e8000137f943bb502f87b2b3b8102d2b4f11
fatb <arno.tgh@gmail.com>
2020-04-25 02:05:10 +0200

    handling errors and pending state

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
   <div class="content">
     <h1 class="text-center">Liste des zones géographiques</h1>
     <v-container fluid>
-      <v-row v-if="areas">
+      <v-row v-if="areas.length > 0">
         <v-col v-for="(area, index) in areas" :key="index" cols="12" sm="6" lg="4">
           <v-card hover>
             <v-card-title class="pb-0">{{ area.name }}</v-card-title>
@@ -23,9 +23,24 @@
           </v-card>
         </v-col>
       </v-row>
-      <div v-else class="text-center">
+      <v-row v-else-if="$fetchState.pending" justify="center">
+        <v-progress-circular
+          indeterminate
+          rotate="4"
+          size="18"
+          width="2"
+          color="secondary"
+        />
+      </v-row>
+      <v-row v-else-if="$fetchState.error" justify="center">
+        <v-col cols="12" class="text-center">
+          <div>Erreur avec le server</div>
+        </v-col>
+        <v-btn class="ml-3" small color="secondary" outlined @click="refresh">Rafraichir</v-btn>
+      </v-row>
+      <v-row v-else justify="center">
         <span>Aucune Zone Géographique enregistrée</span>
-      </div>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -38,7 +53,12 @@ export default {
   },
   data () {
     return {
-      areas: null
+      areas: []
+    }
+  },
+  methods: {
+    refresh () {
+      this.$fetch()
     }
   }
 }
