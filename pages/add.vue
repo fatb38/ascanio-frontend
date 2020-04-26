5571b188d8935e735bc092a30fe825a77adfda39
fatb <arno.tgh@gmail.com>
2020-04-27 00:53:57 +0200

    adding a searchbar to get french cities list API

--- pages/add.vue
+++ pages/add.vue
@@ -1,10 +1,73 @@
 <template>
-  <h1>add area</h1>
+  <div>
+    <h1 class="text-center">Nouvelle zone géographique</h1>
+    <v-container fluid>
+      <v-form ref="form" v-model="valid">
+        <v-row>
+          <v-subheader class="title">Sélectionnez de 1 à 3 villes</v-subheader>
+          <v-autocomplete
+            v-model="select"
+            :loading="loading"
+            :items="items"
+            :search-input.sync="search"
+            color="secondary"
+            class="mx-4"
+            flat
+            hide-no-data
+            hide-details
+            label="Entrez le nom d'une ville"
+            solo-inverted
+          ></v-autocomplete>
+        </v-row>
+      </v-form>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      valid: false,
+      loading: false,
+      entries: [],
+      search: null,
+      select: null,
+      cities: []
+    }
+  },
 
+  computed: {
+    items () {
+      return this.entries.map((entry) => {
+        return entry.nom
+      })
+    }
+  },
+
+  watch: {
+    async search (val) {
+      if (val.length === 0) {
+        this.entries = []
+        return
+      }
+
+      // Items have already been loaded
+      if (this.items.length > 0) {
+        return
+      }
+
+      // Items have already been requested
+      if (this.loading) {
+        return
+      }
+
+      this.loading = true
+      const { data } = await this.$axios.get('https://geo.api.gouv.fr/communes?nom=' + val)
+      this.entries = data
+      this.loading = false
+    }
+  }
 }
 </script>
 
